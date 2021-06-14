db5a1d8f617f9a4f57160f0542ef15b7db270a9b
faliqf <faliqfadlullah@gmail.com>
2021-06-14 13:29:08 +0800

    first commit

---
+++ app/Http/Controllers/ExampleController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Product;
+
+class ExampleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::get();
+
+        return view('example.index', compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('example.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $product = new Product;
+        $product->name = $request->name;
+        $product->save();
+
+        return redirect()->route('example')->with('success', 'Success!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $product = Product::where('id', $id)->first();
+
+        return view('example.edit', compact('product'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $product = Product::where('id', $id)->first();
+
+        $product->name = $request->name;
+        $product->save();
+
+        return redirect()->route('example')->with('success', 'Success!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $product = Product::where('id', $id)->first();
+        $product->delete();
+
+        return redirect()->route('example')->with('success', 'Success!');
+    }
+}
---
+++ app/Http/routes.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('welcome');
+});
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/example', ['as' => 'example', 'uses' => 'ExampleController@index']);
+    Route::get('/example/create', ['as' => 'example-add', 'uses' => 'ExampleController@create']);
+    Route::post('/example/store', ['as' => 'example-store', 'uses' => 'ExampleController@store']);
+    Route::get('/example/edit/{id}', ['as' => 'example-edit', 'uses' => 'ExampleController@edit']);
+    Route::post('/example/update/{id}', ['as' => 'example-update', 'uses' => 'ExampleController@update']);
+    Route::post('/example/delete/{id}', ['as' => 'example-delete', 'uses' => 'ExampleController@destroy']);
+});
