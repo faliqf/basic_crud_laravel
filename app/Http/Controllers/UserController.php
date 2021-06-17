68474037202b1570e952c6b0327856b17a618fce
DESKTOP-OMBS1NL\USER <haziq.zainuddin1@gmail.com>
2021-06-17 10:57:53 +0800

    no message

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\baru;
 
-class ExampleController extends Controller
+class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
